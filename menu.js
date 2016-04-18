7bb41263d231e701fe9c17617f5784e7257258f9
samanthahindes <samantha.hindes@wustl.edu>
2016-04-18 18:24:20 -0500

    menu

--- menu.js
+++ menu.js
@@ -4,12 +4,19 @@ var order = '';
 
 // build a function to ask questions
 
-function question() {
+function question(questionText) {
+  var answerInput= prompt(questionText);
+      order= order + answerInput;
   // your code goes here
 }
 
 // call the QUESTION function with the order options
-question();
+question("Burrito, tacos, bowl, or salad?")
+question("What kind of meat do you want?");
+question("What type of rice do you want?");
+question("What type of beans do you want?");
+question("Do you want any sauces?");
+question("Do you want any other toppings?");
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
